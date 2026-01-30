029aaba80a10962e95bbaf961f919dd336404c4e
hoangfbof-hue <hoangfbof@gmail.com>
2026-01-30 21:45:08 +0700

    Ex5.py

---
+++ Chapter1/Ex5.py
@@ -0,0 +1,13 @@
+name = "Nguyễn Văn A"
+age = 20
+average_score = 8.5
+age_next_year = age + 1
+doubled_score = average_score * 2
+print("--- THÔNG TIN CÁ NHÂN ---")
+print(f"Tên: {name} | Kiểu dữ liệu: {type(name)}")
+print(f"Tuổi hiện tại: {age} | Kiểu dữ liệu: {type(age)}")
+print(f"Điểm trung bình: {average_score} | Kiểu dữ liệu: {type(average_score)}")
+
+print("\n--- THÔNG TIN SAU XỬ LÝ ---")
+print(f"Tuổi vào năm sau: {age_next_year} | Kiểu dữ liệu: {type(age_next_year)}")
+print(f"Điểm sau khi nhân đôi: {doubled_score} | Kiểu dữ liệu: {type(doubled_score)}")
