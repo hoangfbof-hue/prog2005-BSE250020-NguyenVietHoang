f146ca5fcea6702c8d91f451ec248d5b8a05f1c6
hoangfbof-hue <hoangfbof@gmail.com>
2026-01-30 21:43:23 +0700

    Ex4.py

---
+++ Chapter1/Ex4.py
@@ -0,0 +1,10 @@
+def sum_two_numbers(a, b):
+    """
+    Hàm nhận vào hai số a, b và trả về tổng của chúng.
+    """
+    return a + b
+so_thu_nhat = 15
+so_thu_hai = 25
+
+ket_qua = sum_two_numbers(so_thu_nhat, so_thu_hai)
+print(f"Tổng của {so_thu_nhat} và {so_thu_hai} là: {ket_qua}")
