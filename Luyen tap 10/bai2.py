ff310e0df5c30b8b812a396417d0e9b9ff675b76
hoangfbof-hue <hoangfbof@gmail.com>
2026-03-20 16:14:02 +0700

    Create bai2.py

---
+++ Luyen tap 10/bai2.py
@@ -0,0 +1,5 @@
+input_string = input("Nhập vào một chuỗi: ")
+input_char = input("Nhập vào một ký tự cần đếm: ")
+count = input_string.count(input_char)
+
+print(f"Ký tự '{input_char}' xuất hiện {count} lần trong chuỗi.")
