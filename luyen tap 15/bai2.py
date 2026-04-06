499e1b9597c4dd5daad5fb429d90ddaa682ac39c
hoangfbof-hue <hoangfbof@gmail.com>
2026-04-06 17:26:39 +0700

    Create bai2.py

---
+++ luyen tap 15/bai2.py
@@ -0,0 +1,6 @@
+def greet(name="Student"):
+    print(f"Hello, {name}!")
+print("Gọi hàm không có đối số:")
+greet() 
+print("\nGọi hàm có đối số ('Nguyễn Việt Hoàng'):")
+greet("Nguyễn Việt Hoàng")
